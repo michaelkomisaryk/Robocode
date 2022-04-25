b4f013853525da217170b17b5b1badf9cddd71a3
Michael <michael@Dmytroks-MacBook-Pro.local>
2022-04-25 19:32:58 +0300

    learn JavaScript

---
+++ js.js
@@ -0,0 +1,24 @@
+// let a = 2;
+// a = 'word'
+// let ads = [1,2,3,4,6,8]
+// const b = 4;
+// b = 5;
+
+// let getInner = ;
+// console.log(getInner)
+ let temp= prompt("тумпература")
+ //document.getElementById("d").innerHTML = answer;
+
+ function getTemp(){
+  
+
+ if(temp <= 10){
+ alert("So cold");
+   }
+  else if(temp >= 37) {
+    alert ("So hot");
+  }
+  else{
+      alert("normal")
+  }
+ }
